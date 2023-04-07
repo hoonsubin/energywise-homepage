064d665fc0b56bb21b0c5d03e2de3bc98c33f4bc
Hoon Kim <mail@hoonkim.me>
2023-04-07 13:44:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# energywise Landing Page
+
+This repository contains the source code for energywise, a startup project for HM SCE program.
+
+This project uses next.js, typescript, and tailwind.
---
+++ src/templates/Hero.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+import { Logo } from './Logo';
+import { Background } from '../background/Background';
+import { Button } from '../button/Button';
+import { HeroOneButton } from '../hero/HeroOneButton';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+
+const Hero = () => (
+  <Background color="bg-primary-800">
+    <Section yPadding="py-6">
+      <NavbarTwoColumns logo={<Logo xl />}>
+        <li>
+          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+            <a>GitHub</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Sign in</a>
+          </Link>
+        </li>
+      </NavbarTwoColumns>
+    </Section>
+
+    <Section yPadding="pt-20 pb-32">
+      <HeroOneButton
+        title={
+          <>
+            {'The simplest way to get started with your\n'}
+            <span className="text-primary-500">home EV charging station</span>
+          </>
+        }
+        description="Get started to charge your world from your finger tips."
+        button={
+          <Link href="https://creativedesignsguru.com/category/nextjs/">
+            <a>
+              <Button xl>Join the wait list</Button>
+            </a>
+          </Link>
+        }
+      />
+    </Section>
+  </Background>
+);
+
+export { Hero };
