dcacf6960607f02947644e72d121c15e7c156114
energywis3 <129427061+energywis3@users.noreply.github.com>
2023-05-27 22:44:42 +0200

    Update Hero.tsx
    mail function fixed

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
@@ -32,16 +32,23 @@ const Hero = () => {
     };
   }, []);
 
-  const validateEmail = () => {
-    const emailInput = document.getElementById('email');
-    const email = emailInput.value.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [emailInputValue, setEmailInputValue] = useState('');
 
-    if (!emailRegex.test(email)) {
-      alert('Please enter a valid email address.');
-      return false;
-    }
-  };
+  const handleEmailInputChange = useCallback((event) => {
+    setEmailInputValue(event.target.value);
+  }, []);
+
+  const validateEmail = useCallback(
+    (event) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!emailRegex.test(emailInputValue)) {
+        alert('Please enter a valid email address.');
+        event.preventDefault();
+      }
+    },
+    [emailInputValue]
+  );
 
   return (
     <div className="min-h-screen flex flex-col text-white">
@@ -50,9 +57,16 @@ const Hero = () => {
           <Logo xl />
         </div>
         <div className="flex justify-end">
-          <Link href="https://github.com/hoonsubin/energywise-homepage">
-            <a className="text-white">GitHub</a>
-          </Link>
+          <motion.a
+            className="text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+            whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+            whileTap={{ scale: 0.9 }}
+            href="https://github.com/hoonsubin/energywise-homepage"
+          >
+            GitHub
+          </motion.a>
           <Link href="/">
             <a className="ml-4 text-white">Sign in</a>
           </Link>
@@ -64,7 +78,7 @@ const Hero = () => {
             <motion.div
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 1.5 }} // Adjust the duration here (in seconds)
+              transition={{ duration: 1.5 }}
               className="absolute inset-0 z-0 overflow-hidden rounded-lg"
             >
               <Image
@@ -96,15 +110,17 @@ const Hero = () => {
             home EV charging station
           </motion.h1>
           <div className="text-lg md:text-2xl ls:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full survey-button">
-            <a
+            <motion.a
               href="https://energywise.solutions/"
               target="_blank"
               rel="noopener noreferrer"
               className="block w-full h-full rounded-full flex items-center justify-center text-black font-bold text-xl md:text-2xl lg:text-4xl"
               style={{ backgroundColor: '#E4FF1A', padding: '1rem' }}
+              whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+              whileTap={{ scale: 0.9 }}
             >
               Take Survey
-            </a>
+            </motion.a>
           </div>
           <form action="------" method="post" target="_blank">
             <div className="flex flex-col md:flex-row justify-center items-center mb-4">
@@ -115,6 +131,8 @@ const Hero = () => {
                 name="member[email]"
                 className="text-lg md:text-2xl placeholder:text-gray-400 placeholder-italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10
                 focus:bg-opacity-20 duration-150 rounded-full md:rounded-tr-none md:rounded-br-none mb-4 md:mb-0 outline-none"
+                value={emailInputValue}
+                onChange={handleEmailInputChange}
               />
               <div className="flex-shrink-0">
                 <a
@@ -157,4 +175,5 @@ const Hero = () => {
     </div>
   );
 };
+
 export default Hero;
