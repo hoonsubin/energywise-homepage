a277fa4d3783b4676631fd91e0e27a792ab78141
energywis3 <129427061+energywis3@users.noreply.github.com>
2023-07-02 18:50:10 +0200

    Delete src directory

--- src/utils/AppConfig.ts
+++
@@ -1,7 +0,0 @@
-export const AppConfig = {
-  site_name: 'energywise',
-  title: 'energywise | The EVolution of charging',
-  description:
-    'Get started to charge your world from your finger tips with energywise.',
-  locale: 'en',
-};
