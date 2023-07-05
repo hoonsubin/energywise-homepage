5408d1523ce2a34379990d6b24ecfe2dabe0b9e2
Hoon Kim <mail@hoonkim.me>
2023-07-05 20:26:39 +0200

    cta to link main app (#28)

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './CTA.css'
 
 const CTA = () => {
-  const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSeIyYyHaHuQaF5UmlyT-HlWWsHyGgonRSfgtNLhdQ88JcKEaw/viewform?embedded=true';
+  const formUrl = 'https://app.energywise.solutions/#/survey';
 
   return (
     <div className='cta'>
