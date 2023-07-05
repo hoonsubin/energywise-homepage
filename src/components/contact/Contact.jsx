3f7eb648fa9461502460e25ffd44030a14ce5227
energywis3 <129427061+energywis3@users.noreply.github.com>
2023-07-05 20:51:08 +0200

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>team@energywisee.solutions</h5>
+            <h5>team@energywise.solutions</h5>
             <a href="mailto:team@energywise.solutions" target="_blank" rel="noopener noreferrer">
               Send a message
             </a>
